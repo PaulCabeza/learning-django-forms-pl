bf1b53b2732ae5426b0a02e86782eaeeb00e71bd
Paul Cabeza <ovidio.cabeza@gmail.com>
2021-10-23 08:42:29 -0600

    commenting form model not used

--- website/forms.py
+++ website/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from django.forms import ModelForm, fields
 from .models import User
 
-class NameForm(forms.Form):
-    name = forms.CharField(max_length=100, label='Your Name')
-    def save(self):
-        pass
+# class NameForm(forms.Form):
+#     name = forms.CharField(max_length=100, label='Your Name')
+#     def save(self):
+#         pass
 
 class UserForm(ModelForm):
     class Meta:
